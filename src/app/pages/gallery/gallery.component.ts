bc1038e114c517ec5f93a105b4938172e62ef870
drako9159 <chochito9159@gmail.com>
2024-02-15 22:19:56 -0600

    feat(gallery): configure gallery component

--- src/app/pages/gallery/gallery.component.ts
+++ src/app/pages/gallery/gallery.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PhotoComponent } from './photo/photo.component';
+import { Photo } from '../../models/photo';
 
 @Component({
   selector: 'app-gallery',
   standalone: true,
-  imports: [],
+  imports: [PhotoComponent],
   templateUrl: './gallery.component.html',
-  styleUrl: './gallery.component.css'
+  styleUrl: './gallery.component.css',
 })
-export class GalleryComponent {
-
+export class GalleryComponent implements OnInit {
+  photoTest = new Photo();
+  ngOnInit(): void {
+    this.photoTest.url =
+      'https://cdn.pixabay.com/photo/2013/07/18/10/56/railroad-163518_1280.jpg';
+  }
 }
--- src/app/pages/gallery/photo/photo.component.ts
+++ src/app/pages/gallery/photo/photo.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Photo } from '../../../models/photo';
 
 @Component({
   selector: 'app-photo',
   standalone: true,
   imports: [],
   templateUrl: './photo.component.html',
-  styleUrl: './photo.component.css'
+  styleUrl: './photo.component.css',
 })
 export class PhotoComponent {
-
+  @Input()
+  photo!: Photo;
 }
